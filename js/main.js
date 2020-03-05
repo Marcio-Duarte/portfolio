9fa990c2f9dd607b044e35687b3ddf559b27182f
Marcio <marcio-duarte@hotmail.com>
2020-03-05 21:36:22 +0100

    shake it implemented

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+/* $(document).ready(function () {
+
+    $("img").click(function () {
+        $("img").effect("shake", { times: 4 }, 1000);
+    });
+
+}); */
+
+
+/* $('img').click(function () {
+    $(this).effect("shake");
+    alert("22222");
+}); */
