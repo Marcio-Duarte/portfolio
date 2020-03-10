07180bb7db7e4df2e40b466c152edadad765f6cc
Marcio Duarte <mduarte@marcio.dci>
2020-03-10 16:24:35 +0100

    animation added to the toggle btn

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
 
     $(".navbar ul:first-of-type li a").click(function () {
-        $(".navbar ul:first-of-type li a").removeClass('active');
-        $(this).toggleClass('active');
+        $(".navbar ul:first-of-type li a").removeClass('active-link');
+        $(this).toggleClass('active-link');
+    });
+
+    /* Navbar toggle button effect */
+    $('#navbar-toggle-btn').click(function () {
+        if (!$(this).hasClass('navbar-toggle-active')) {
+            $(this).toggleClass('navbar-toggle-active');
+            $(this).removeClass('navbar-toggle-inactive');
+        } else {
+            $(this).addClass('navbar-toggle-inactive');
+            $(this).removeClass('navbar-toggle-active');
+            setTimeout(function () {
+                $('#navbar-toggle-btn').removeClass('navbar-toggle-inactive');
+            }, 1000);
+        }
     });
 
 });
