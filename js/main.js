44fc2e277899aa4579d869d2ff1439ae67150b90
Marcio Duarte <mduarte@marcio.dci>
2020-03-11 16:18:28 +0100

    side navbar added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+    /* *** Navbar and side navbar elements and actions *** */
 
-    /* *** Navbar elements and actions *** */
-    /* Set active menu */
-    $(".navbar ul:first-of-type li a").click(function () {
-        $(".navbar ul:first-of-type li a").removeClass('active-link');
-        $(this).toggleClass('active-link');
+    /* Copy contents to the side navbar from the main navbar */
+    let navMenu = document.querySelectorAll('.navbar-container')[0].cloneNode(true);
+    let sideNavbar = document.getElementById("side-navbar");
+    navMenu.classList.remove("navbar-container");
+    navMenu.classList.add("side-navbar-container");
+    navMenu.childNodes[1].classList.remove("navbar-item");
+    navMenu.childNodes[1].classList.add("side-navbar-item");
+    sideNavbar.appendChild(navMenu);
+
+
+    /* Set active menu in both bars at the same time*/
+    $(".navbar ul:first-of-type li a, .sidenav ul:first-of-type li a").click(function (e) {
+        $(".navbar ul:first-of-type li a , .sidenav ul:first-of-type li a").removeClass('active-link');
+        $(".navbar ul:first-of-type li a[name=" + "'" + e.currentTarget.name + "'" + "]").addClass('active-link');
+        $(".sidenav ul:first-of-type li a[name=" + "'" + e.currentTarget.name + "'" + "]").addClass('active-link');
     });
 
     /* Navbar toggle button effect */
@@ -12,13 +23,22 @@ $(document).ready(function () {
         if (!$(this).hasClass('navbar-toggle-active')) {
             $(this).toggleClass('navbar-toggle-active');
             $(this).removeClass('navbar-toggle-inactive');
+            setSideNavbar();
         } else {
             $(this).addClass('navbar-toggle-inactive');
             $(this).removeClass('navbar-toggle-active');
+            setSideNavbar();
             setTimeout(function () {
                 $('#navbar-toggle-btn').removeClass('navbar-toggle-inactive');
-            }, 300);
+            }, 500);
         }
     });
-
+    function setSideNavbar() {
+        if ($('#navbar-toggle-btn').hasClass('navbar-toggle-active')) {
+            sideNavbar.style.width = "120px";
+        } else {
+            sideNavbar.style.width = "0";
+        }
+    }
 });
+
