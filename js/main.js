52f01c5bfaea84c10dc4deaa2e6b240641c8c5b8
Marcio <marcio-duarte@hotmail.com>
2020-03-26 14:13:35 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,114 @@
 $(document).ready(function () {
+
+    /* var modal = document.getElementById("modal-photo"); */
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    $("#navbar-photo").click(function () {
+        let modal = $('#modal-photo');
+        modal.css({ "display": "flex" });
+
+        /* $('body').css({
+            overflow: 'hidden',
+            height: '100%'
+        }); */
+
+        $(modal).children().click(function () {
+            modal.css({ "display": "none" });
+            /*  $('body').css({
+                 overflow: 'scroll',
+                 height: '100%'
+             }); */
+        });
+
+        /*  var modal = document.getElementById("modal-photo");
+         let img = $(this).children(); */
+        /* $(modal).attr('display', 'flex');
+        $(modal).addClass('showModal'); */
+        /* modal.style.display = "flex"; */
+
+
+        /* $(selector).attr(attributeName); */
+        /* modal.style.display = "block";
+        modalImg.src = img.attr('src');
+        captionText.innerHTML = "this.alt";
+
+        $(modal).click(function (e) {
+            modal.style.display = "none";
+
+        }); */
+    });
+
+    // Get the <span> element that closes the modal
+    /* var span = document.getElementsByClassName("close")[0]; */
+
+    // When the user clicks on <span> (x), close the modal
+    /*  span.onclick = function () {
+         modal.style.display = "none";
+     } */
+
+
+    /*  var modal = document.getElementById("myModal"); */
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    /* var img = document.getElementById("navbar-photo");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function () {
+        console.log(img);
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+ */
+
+
+
+
+
+
+
+
     /* *** Navbar and side navbar elements and actions *** */
 
-    $('#navbar-photo').click(function () {
-        $(this).css("position", "absolute").css('top', '100px');
-        /* let t = document.getElementById("side-navbar");
-        t.style.width = '200px'; */
-    });
+    /*  $('#navbar-photo').click(function () {
+         let t = $(this).clone();
+         t.removeClass("rounded-border").children().removeClass("rounded-border")
+         t.css({
+             "position": "absolute",
+             "transition": "0.6s",
+             "top": "50%"
+         });
+ 
+ 
+     console.log(t.position);
+     $('body').append(t); */
+    /* $(this).css("position", "absolute").css('top', '100px'); */
+    /* let t = document.getElementById("side-navbar");
+    t.style.width = '200px'; */
+
+
+    /*  var i = 0;
+     let interval = setInterval(time, 0.1);
+     function time() {
+         i++
+         if (i < 50) {
+             t.css({
+                 'top': `${i}%`,
+                 'left': `${i}%`
+             });
+ 
+         } else {
+             clearInterval(interval);
+         }
+         console.log(i);
+     }
+ 
+     time(); */
+
+    /* }); */
+
+    /* img.style.transform = `rotate(${i}deg)`; */
+
 
     /* Copy contents from the main navbar to the side navbar */
     let sideNavbar = document.getElementById("side-navbar");
@@ -19,16 +122,16 @@ $(document).ready(function () {
 
     /* Navbar toggle button effect */
     $('#navbar-toggle-btn').click(function () {
-        if (!$(this).hasClass('navbar-toggle-active')) {
-            $(this).toggleClass('navbar-toggle-active');
-            $(this).removeClass('navbar-toggle-inactive');
+        if (!$(this).hasClass('burger-btn-active')) {
+            $(this).toggleClass('burger-btn-active');
+            $(this).removeClass('burger-btn-inactive');
             sideNavbar.style.width = "100%";
         } else {
-            $(this).addClass('navbar-toggle-inactive');
-            $(this).removeClass('navbar-toggle-active');
+            $(this).addClass('burger-btn-inactive');
+            $(this).removeClass('burger-btn-active');
             sideNavbar.style.width = "0";
             setTimeout(function () {
-                $('#navbar-toggle-btn').removeClass('navbar-toggle-inactive');
+                $('#navbar-toggle-btn').removeClass('burger-btn-inactive');
             }, 500);
         }
     });
